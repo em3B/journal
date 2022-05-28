5881550ab43ca0dbfa454ba0bb670b58cf1ba182
Emily Baird <emilym.baird@gmail.com>
2022-05-28 23:41:49 +0100

    login save details fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_15_204538) do
+ActiveRecord::Schema.define(version: 2022_05_28_214309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,8 @@ ActiveRecord::Schema.define(version: 2022_05_15_204538) do
     t.integer "sign_in_count"
     t.datetime "remember_created_at"
     t.datetime "confirmed_at"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
   end
 
   add_foreign_key "answers", "groups"
